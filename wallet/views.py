41c113563bd7e75844728cc7174e4d4f5fe9c38a
Artumos_Goc <63540372+ArtumosGOC@users.noreply.github.com>
2024-06-22 11:43:51 -0300

    updating view wallet

--- wallet/views.py
+++ wallet/views.py
@@ -5,5 +5,5 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def history_profile(request):
     data = Activities.objects.filter(user=request.user).order_by('-date')
-    return render(request, 'wallet/wallet_profile.html', {'activities': data})
+    return render(request, 'wallet/wallet_profile.html', {'data': data})
 
