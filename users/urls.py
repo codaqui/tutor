01a0b6548d28050e0519f928ef792b7c143d3bb8
Artumos_Goc <63540372+ArtumosGOC@users.noreply.github.com>
2024-07-15 22:17:46 -0300

    Feat: Add new Urls

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from users import views
+from users.apps import UsersConfig 
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path('', views.user_create_repository, name='repository'),
+]
