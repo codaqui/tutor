eb0560545136f8ba56317914d8a0949e0a18f5f4
Artumos_Goc <63540372+ArtumosGOC@users.noreply.github.com>
2024-08-19 21:48:19 -0300

    feat: Add DC_TOKEN environment variable
    The code changes include adding the DC_TOKEN environment variable to the settings.py file. This variable is used for Discord authentication and message sending functionality in the application.

--- codaqui/settings.py
+++ codaqui/settings.py
@@ -145,6 +145,7 @@ STATIC_URL = "static/"
 GH_PRIVATE_KEY_FILE = os.getenv("GH_PRIVATE_KEY_FILE")
 GH_APP_INSTALL_ID = os.getenv("GH_APP_INSTALL_ID")
 GH_APP_ID = os.getenv("GH_APP_ID")
+DC_TOKEN = os.getenv("DC_TOKEN")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
