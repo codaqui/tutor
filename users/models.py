795cd10ccd99a9cec0c0f977ba56d7432c7a3f1c
Enderson Menezes <endersonster@gmail.com>
2025-01-07 20:22:38 -0300

    refactor: Add type hints to GitHub data retrieval methods in User model

--- users/models.py
+++ users/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
-    def get_github_data(self):
+    def get_github_data(self) -> dict:
         return self.social_auth.get(provider="github")
 
-    def get_github_username(self):
+    def get_github_username(self) -> str:
         return self.get_github_data().extra_data.get("login")
