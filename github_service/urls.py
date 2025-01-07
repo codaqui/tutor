1cacfbb925de484abd7e348f3055f7f0d69c4ab3
Enderson Menezes <endersonster@gmail.com>
2025-01-07 20:22:48 -0300

    feat: Add URL configuration for GitHub service with issue listing and retrieval endpoints

---
+++ github_service/urls.py
@@ -0,0 +1,28 @@
+"""
+URL configuration for codaqui project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import path
+from django.contrib import admin
+from github_service.apps import GithubServiceConfig
+from github_service.views import view_list_issues, view_get_issue
+
+app_name = GithubServiceConfig.name
+
+urlpatterns = [
+    path("list-issues/", view_list_issues, name="list_issues"),
+    path("get-issue/<int:issue_number>/<str:action>/", view_get_issue, name="get_issue"),
+]
